a3cbe2c2c205805c8e3ef9b211648e3abcfda35b
Unknown <s10173143@connect.np.edu.sg>
2017-07-28 20:09:49 +0800

    add response

---
+++ ZZ_Fashion/LoginPages/Customer/AppCode/Response.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZZ_Fashion.LoginPages.Customer.AppCode
+{
+    public class Response
+    {
+    }
+}
